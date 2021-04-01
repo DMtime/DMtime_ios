e9860a17ab6afcf160637a186867f0f9ff7d5873
ohjunsang <pandati@dsm.hs.kr>
2021-04-01 10:55:08 +0900

    [ADD] main page에 서버 연결

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -2,14 +2,19 @@ import React from "react";
 import { ScrollView } from "react-native";
 import BoardList from "./boardList";
 import S from "./style";
+import useBoardListUseCase from "../../hooks/useCase/board/useBoardListUseCase";
+import { board } from "../../models/board";
 
 const Main = () => {
+  const { boardList } = useBoardListUseCase(true);
+  console.log(boardList);
+  const renderBoardList = (boardList: board[]) =>
+    boardList.map((board) => (
+      <BoardList boardListTitle={board.name} boardId={board.id} />
+    ));
   return (
     <ScrollView style={S.Main}>
-      <BoardList boardListTitle="hello" />
-      <BoardList boardListTitle="hello" />
-      <BoardList boardListTitle="hello" />
-      <BoardList boardListTitle="hello" />
+      {renderBoardList(boardList ? boardList : [])}
     </ScrollView>
   );
 };
--- src/components/main/boardList/BoardList.tsx
+++ src/components/main/boardList/BoardList.tsx
@@ -1,20 +1,26 @@
 import React, { FC } from "react";
 import { View } from "react-native";
 import S from "../style";
+import usePostListUseCase from "../../../hooks/useCase/post/usePostListUseCase";
 import BoardListHeader from "./BoardListHeader";
 import BoardListItem from "./BoardListItem";
+import { post } from "../../../models/board";
 
 interface Props {
   boardListTitle: string;
+  boardId: number;
 }
 
-const BoardList: FC<Props> = ({ boardListTitle }) => {
+const BoardList: FC<Props> = ({ boardListTitle, boardId }) => {
+  const { postList } = usePostListUseCase(boardId);
+  const renderPostList = (postList: post[]) =>
+    postList.map((post: post) => (
+      <BoardListItem date={post.posted_datetime} title={post.title} />
+    ));
   return (
     <View style={S.MainBoardList}>
       <BoardListHeader title={boardListTitle} />
-      <BoardListItem date="3/18" title="오준상이 만든 react-native" />
-      <BoardListItem date="3/18" title="오준상이 만든 react-native" />
-      <BoardListItem date="3/18" title="오준상이 만든 react-native" />
+      {renderPostList(postList)}
     </View>
   );
 };
