dbe7e7733005b60de695d16ccb5a5c4a7ecdffa5
ohjunsang <pandati@dsm.hs.kr>
2021-04-13 21:38:35 +0900

    [ADD] image 추가

--- src/components/postDetail/body/PostDetailBody.tsx
+++ src/components/postDetail/body/PostDetailBody.tsx
@@ -1,15 +1,26 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import { Text, View } from "react-native";
 import S from "../style";
+import PostImage from "./Image/PostImage";
 
 interface Props {
   content: string;
+  imageUrls: string[];
+  setModal: (value: boolean) => void;
 }
 
-const PostDetailBody: FC<Props> = ({ content }) => {
+const PostDetailBody: FC<Props> = ({ content, imageUrls, setModal }) => {
+  const onClick = () => {
+    setModal(true);
+  };
   return (
     <View style={S.Body}>
       <Text style={S.Content}>{content}</Text>
+      {imageUrls.length > 0 ? (
+        <PostImage imageUrls={imageUrls} onClick={onClick} />
+      ) : (
+        <View />
+      )}
     </View>
   );
 };
