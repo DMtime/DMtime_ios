c788d4f886a2468b14bfba777fd9a8ee25283c99
ohjunsang <pandati@dsm.hs.kr>
2021-04-13 21:41:34 +0900

    [ADD] 댓글 작성 추가

--- src/components/postDetail/input/Input.tsx
+++ src/components/postDetail/input/Input.tsx
@@ -1,17 +1,46 @@
-import React, { FC } from "react";
-import { View, TextInput, TouchableOpacity, Text } from "react-native";
+import React, { FC, useState } from "react";
+import {
+  View,
+  TextInput,
+  TouchableOpacity,
+  Text,
+  TextInputChangeEventData,
+  NativeSyntheticEvent,
+} from "react-native";
 import S from "../style";
 
-const Input: FC = () => {
+interface Props {
+  addComment: (
+    isAnonymous: boolean,
+    content: string,
+    upperCaseCommentId?: number
+  ) => void;
+  upperCaseCommentId?: number;
+}
+
+const Input: FC<Props> = ({ addComment, upperCaseCommentId }) => {
+  const [content, setContent] = useState<string>("");
+  const submitButtonPressHandler = () => {
+    addComment(true, content, upperCaseCommentId);
+  };
+  const inputChangeHandler = (
+    e: NativeSyntheticEvent<TextInputChangeEventData>
+  ) => {
+    setContent(e.nativeEvent.text);
+  };
   return (
     <View style={S.CommentWriteInputWrapper}>
       <TextInput
         multiline={true}
         style={S.CommentWriteInput}
         placeholder="댓글을 입력하세요."
+        onChange={inputChangeHandler}
       />
       <View style={S.CommentWriteButtonWrapper}>
-        <TouchableOpacity onPress={() => {}} style={S.CommentWriteButton}>
+        <TouchableOpacity
+          onPress={submitButtonPressHandler}
+          style={S.CommentWriteButton}
+        >
           <Text style={S.CommentWriteButtonText}>등록</Text>
         </TouchableOpacity>
       </View>
