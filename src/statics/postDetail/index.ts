04d9b859f701ac3ef063095052781803e1a88d15
ohjunsang <pandati@dsm.hs.kr>
2021-04-13 21:50:10 +0900

    [ADD] image 추가

--- src/statics/postDetail/index.ts
+++ src/statics/postDetail/index.ts
@@ -2,3 +2,4 @@ export { default as dislikeImage } from "./dislike.png";
 export { default as likeImage } from "./like.png";
 export { default as reportImage } from "./report.png";
 export { default as defaultUserImage } from "./defaultUser.png";
+export { default as leftArrowImage } from "./leftArrow.png";
