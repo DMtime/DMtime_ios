c9a81589c3149157ca2d33c99420c551669a69c1
ohjunsang <pandati@dsm.hs.kr>
2021-04-13 21:50:04 +0900

    [FIX] domain 수정

--- src/models/board/index.ts
+++ src/models/board/index.ts
@@ -2,21 +2,27 @@ export type board = {
   name: string;
   id: number;
   explain: string;
+  gellary_type: 0 | 1 | 2;
+  gellary_id: string;
 };
 
 export type uploader = {
   username: string;
 };
 
+export type image = Record<string, string>;
+
 export type post = {
   content: string;
   is_anonymous: boolean;
   id: number;
-  image_ids: string[];
-  likes: number;
+  image_ids: image;
+  number_of_likes: number;
+  number_of_dislikes: number;
   posted_datetime: string;
   posted_gallery: board;
   title: string;
   uploader: uploader;
   views: number;
+  my_reaction: "none" | "like" | "dislike";
 };
