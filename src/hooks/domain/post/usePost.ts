c462ea73ec9a5992a1415ebb87258893ce7603f5
ohjunsang <pandati@dsm.hs.kr>
2021-04-13 21:48:12 +0900

    [FIX] board랑 Post 기본 state 저장 ( 임시 )
    [

--- src/hooks/domain/post/usePost.ts
+++ src/hooks/domain/post/usePost.ts
@@ -1,8 +1,22 @@
 import { useState } from "react";
-import { post } from "../../../models/board";
+import { board, post } from "../../../models/board";
 
 const usePost = () => {
-  const [post, setPost] = useState<post>();
+  const [post, setPost] = useState<post>({
+    content: "대마고는 대덕공고다",
+    is_anonymous: false,
+    id: 1,
+    image_ids: {},
+    likes: 1,
+    posted_datetime: "4/6",
+    posted_gallery: {} as board,
+    title: "테스트",
+    uploader: {
+      username: "오준상",
+    },
+    views: 1,
+    my_reaction: "none",
+  });
   return {
     post,
     setPost,
--- src/hooks/domain/post/usePostList.ts
+++ src/hooks/domain/post/usePostList.ts
@@ -1,8 +1,23 @@
 import { useState } from "react";
-import { post } from "../../../models/board";
+import { board, post } from "../../../models/board";
 
 const usePostList = (id: number) => {
-  const [postList, setPostList] = useState<post[]>([]);
+  const [postList, setPostList] = useState<post[]>([
+    {
+      content: "string",
+      is_anonymous: false,
+      id: 1,
+      image_ids: {},
+      likes: 1,
+      posted_datetime: "string",
+      posted_gallery: {} as board,
+      title: "string",
+      uploader: {
+        username: "오준상",
+      },
+      views: 1,
+    },
+  ]);
   const [page, setPage] = useState<number>(0);
   return {
     postList,
