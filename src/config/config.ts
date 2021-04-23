f9cf326cb2830e2107a4ba2969b7acae29cf2c10
ohjunsang <pandati@dsm.hs.kr>
2021-04-23 12:03:21 +0900

    [FIX] 서버 주소 수정 및 image default link 추가

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,5 +1,6 @@
 const config = {
-  BASE_URL: "http://dminside.kro.kr/api/v1",
+  BASE_URL: "https://api.dmtime.xyz/api/v1",
+  IMAGE_URL: "https://dmtimebucket.s3.ap-northeast-2.amazonaws.com/images/",
 };
 
 export default config;
