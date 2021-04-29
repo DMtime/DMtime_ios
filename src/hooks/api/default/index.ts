87d252e629141e3895f0e8e091b6140efa447ce8
ohjunsang <pandati@dsm.hs.kr>
2021-04-29 12:03:28 +0900

    [DELETE] dummy data 삭제

--- src/hooks/api/default/index.ts
+++ src/hooks/api/default/index.ts
@@ -3,10 +3,6 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import config from "../../../config/config";
 
 export const getRequestWithAccessToken = async () => {
-  AsyncStorage.setItem(
-    "accessToken",
-    "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2MTkxMzk3OTksIm5iZiI6MTYxOTEzOTc5OSwianRpIjoiY2Y2ZDQxZmItNTc4YS00YTY3LWI4NWUtMDY1NmI3YjZhMjNjIiwiZXhwIjoxNjE5MTgyOTk5LCJpZGVudGl0eSI6InBhbmRhdGlAZHNtLmhzLmtyIiwiZnJlc2giOmZhbHNlLCJ0eXBlIjoiYWNjZXNzIiwidXNlcl9jbGFpbXMiOnsicm9sZXMiOiJwZWFzYW50In19.My701_ttimU09ekydPASb33mMDJPRoPy8IYu_rhn4Qo"
-  );
   const accessToken = await AsyncStorage.getItem("accessToken");
   return axios.create({
     baseURL: config.BASE_URL,
